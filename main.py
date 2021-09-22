8bf3c174f7460b692ac15a6f1072da90cd00226b
Akash Rao <rao28271@gmail.com>
2021-09-22 21:30:26 +0530

    map_tile

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return redirect('https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=N46AqYQ1bpAbqASUk_nsK-sMDNkFh5lkeAA2DFkPDU8', code=302)
+    return redirect('https://1.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/13/4400/2686/256/png8?apiKey=N46AqYQ1bpAbqASUk_nsK-sMDNkFh5lkeAA2DFkPDU8', code=302)
 
 if __name__ == '__main__':
  

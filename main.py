0060c9700b36ee5743c30f42f85d0262f94e5023
Akash Rao <rao28271@gmail.com>
2021-09-22 23:20:02 +0530

    added_longitude_&_lattitude

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import requests
 import json
 from flask import Flask,redirect
 import urllib.request
+import math
 
-#response = requests.get("https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=N46AqYQ1bpAbqASUk_nsK-sMDNkFh5lkeAA2DFkPDU8")
-#print(response.json())
+lat_py = float(input("Latitude:"))
+lon_py = float(input("Longitude:"))
+
+lat = lat_py #lattitude
+lon = lon_py #longitude
+z = 13 		 #zoom
+latRad = lat * math.pi / 180
+n = pow(2, z)
+xTile = n * ((lon + 180) / 360)
+yTile = n * (1-(math.log(math.tan(latRad) + 1/math.cos(latRad)) /math.pi)) / 2
 
 
 app = Flask(__name__)
@@ -12,7 +21,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return redirect('https://1.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/13/4400/2686/256/png8?apiKey=N46AqYQ1bpAbqASUk_nsK-sMDNkFh5lkeAA2DFkPDU8', code=302)
+    return redirect('https://1.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/13/'+str(int(xTile))+'/'+str(int(yTile))+'/512/png8?apiKey=N46AqYQ1bpAbqASUk_nsK-sMDNkFh5lkeAA2DFkPDU8', code=302)
 
 if __name__ == '__main__':
  

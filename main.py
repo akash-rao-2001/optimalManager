c5ca176910affe2749e15766ffaf71406809e3a8
Akash Rao <rao28271@gmail.com>
2021-09-25 06:43:40 +0530

    added 5 markers

--- main.py
+++ main.py
@@ -30,31 +30,80 @@ with urlopen('https://discover.search.hereapi.com/v1/discover?at='+str(lat)+','+
 	project_info = json.load(resp)#['title']
 pprint.pprint(project_info)
 
+
 p1title = project_info['items'][0]['title']
-p1phone = project_info['items'][0]['contacts'][0]['phone'][0]['value']
+try: 
+	p1phone = project_info['items'][0]['contacts'][0]['phone'][0]['value']
+except: 
+	pass
+try: 
+	p1phone = project_info['items'][0]['contacts'][0]['mobile'][0]['value']
+except: 
+	pass
+
 p1lat = project_info['items'][0]['access'][0]['lat']
 p1lng = project_info['items'][0]['access'][0]['lng']
 
 p2title = project_info['items'][1]['title']
-p2phone = project_info['items'][1]['contacts'][0]['phone'][0]['value']
+try: 
+	p2phone = project_info['items'][1]['contacts'][0]['phone'][0]['value']
+except: 
+	pass
+try: 
+	p2phone = project_info['items'][1]['contacts'][0]['mobile'][0]['value']
+except: 
+	pass
 p2lat = project_info['items'][1]['access'][0]['lat']
 p2lng = project_info['items'][1]['access'][0]['lng']
 
 p3title = project_info['items'][2]['title']
-p3phone = project_info['items'][2]['contacts'][0]['phone'][0]['value']
+try:
+	p3phone = project_info['items'][2]['contacts'][0]['phone'][0]['value']
+except:
+	pass
+try:
+	p3phone = project_info['items'][2]['contacts'][0]['mobile'][0]['value']
+except:
+	pass
 p3lat = project_info['items'][2]['access'][0]['lat']
 p3lng = project_info['items'][2]['access'][0]['lng']
 
 p4title = project_info['items'][3]['title']
-p4phone = project_info['items'][3]['contacts'][0]['phone'][0]['value']
+try:
+	p4phone = project_info['items'][3]['contacts'][0]['phone'][0]['value']
+except:
+	pass
+try:
+	p4phone = project_info['items'][3]['contacts'][0]['mobile'][0]['value']
+except:
+	pass
 p4lat = project_info['items'][3]['access'][0]['lat']
 p4lng = project_info['items'][3]['access'][0]['lng']
 
 p5title = project_info['items'][4]['title']
-p5phone = project_info['items'][4]['contacts'][0]['phone'][0]['value']
+try:
+	p5phone = project_info['items'][4]['contacts'][0]['phone'][0]['value']
+except:
+	pass
+try:
+	p5phone = project_info['items'][4]['contacts'][0]['mobile'][0]['value']
+except:
+	pass
+
 p5lat = project_info['items'][4]['access'][0]['lat']
 p5lng = project_info['items'][4]['access'][0]['lng']
 
+p6title = project_info['items'][5]['title']
+try:
+	p6phone = project_info['items'][5]['contacts'][0]['phone'][0]['value']
+except:
+	pass
+try:
+	p6phone = project_info['items'][4]['contacts'][0]['mobile'][0]['value']
+except:
+	pass
+p6lat = project_info['items'][5]['access'][0]['lat']
+p6lng = project_info['items'][5]['access'][0]['lng']
 
 
 
@@ -63,7 +112,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-	data = {'x': float(lat), 'y': float(lon), 'p1title': p1title , 'p1phone': p1phone, "p1lat": float(p1lat), "p1lng": float(p1lng), 'p2title': p2title , 'p2phone': p2phone, "p2lat": float(p2lat), "p2lng": float(p2lng), 'p3title': p3title , 'p3phone': p3phone, "p3lat": float(p3lat), "p3lng": float(p3lng), 'p4title': p4title , 'p4phone': p4phone, "p4lat": float(p4lat), "p4lng": float(p4lng), 'p5title': p5title , 'p5phone': p5phone, "p5lat": float(p5lat), "p5lng": float(p5lng)}
+	data = {'x': float(lat), 'y': float(lon), 'p1title': p1title , 'p1phone': p1phone, "p1lat": float(p1lat), "p1lng": float(p1lng), 'p2title': p2title , 'p2phone': p2phone, "p2lat": float(p2lat), "p2lng": float(p2lng), 'p3title': p3title , 'p3phone': p3phone, "p3lat": float(p3lat), "p3lng": float(p3lng), 'p4title': p4title , 'p4phone': p4phone, "p4lat": float(p4lat), "p4lng": float(p4lng), 'p5title': p5title , 'p5phone': p5phone, "p5lat": float(p5lat), "p5lng": float(p5lng), 'p6title': p6title , 'p6phone': p6phone, "p6lat": float(p6lat), "p6lng": float(p6lng)}
 	return render_template('index.html', data=data)
     #return redirect('https://1.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/13/'+str(int(xTile))+'/'+str(int(yTile))+'/512/png8?apiKey=N46AqYQ1bpAbqASUk_nsK-sMDNkFh5lkeAA2DFkPDU8', code=302)
 
